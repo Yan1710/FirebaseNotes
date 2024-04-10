580526b6be136b166c8a4cb997433d9761038c5e
yan_ortiz <yanortiz@wposs.com>
2024-04-10 10:51:00 -0500

    App movil de firebase

---
+++ app/src/main/java/com/example/firebasenotes/components/Alert.kt
@@ -0,0 +1,8 @@
+package com.example.firebasenotes.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Alert(){
+
+}
---
+++ app/src/main/java/com/example/firebasenotes/views/login/LoginView.kt
@@ -0,0 +1,9 @@
+package com.example.firebasenotes.views.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginView(){
+    Text(text = "LoginView")
+}
