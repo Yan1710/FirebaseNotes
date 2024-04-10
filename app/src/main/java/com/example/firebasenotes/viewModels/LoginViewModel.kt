42226df0a7816b16b8a818ee36b59f753ff1ea95
yan_ortiz <yanortiz@wposs.com>
2024-04-10 16:53:56 -0500

    Implementacion para guarda el usuario en firebase

--- app/src/main/java/com/example/firebasenotes/viewModels/LoginViewModel.kt
+++ app/src/main/java/com/example/firebasenotes/viewModels/LoginViewModel.kt
@@ -6,9 +6,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.firebasenotes.model.UserModel
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 
 class LoginViewModel:ViewModel() {
@@ -40,15 +42,16 @@ class LoginViewModel:ViewModel() {
 
 
 
-    fun createUser(email:String,password:String,onSuccess:() -> Unit){
+    fun createUser(email:String,password:String,username: String,onSuccess:() -> Unit){
         viewModelScope.launch {
             try {
                 auth.createUserWithEmailAndPassword(email,password)
                     .addOnCompleteListener{ task ->
                         if(task.isSuccessful){
+                            saveuser(username)
                             onSuccess()
                         }else{
-                            Log.d("ERORR", "login: Usario ")
+                            Log.d("ERORR", "error al crear el usuario ")
                             showAlert = true
                         }
 
@@ -56,6 +59,23 @@ class LoginViewModel:ViewModel() {
             }catch (e:Exception) {
                 Log.d("ERROR",e.message.toString())
             }
+        }
+    }
+
+    private fun saveuser(username:String){
+        val id = auth.currentUser?.uid
+        val email = auth.currentUser?.email
+
+        val user = UserModel(
+            userId = id.toString(),
+            email = email.toString(),
+            username = username
+        )
+
+        FirebaseFirestore.getInstance().collection("Users").add(user).addOnSuccessListener{
+            Log.d("GUARDo", "saveuser: ")
+        }.addOnFailureListener{
+            Log.d("ERROR al GUARDAR", "saveuser: ")
         }
     }
 
